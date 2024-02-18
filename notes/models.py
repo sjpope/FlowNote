249ddf5e4fc86d2c477d10d492de452100483c42
Samuel Pope <spope@inrsenterprises.com>
2024-02-18 01:01:13 -0600

    Auth views, templates, forms implemented. Error in validating RegistrationForm data.

--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notes', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render, redirect
-# from rest_framework import viewsets
-from .models import Note, BlogPost
-from .forms import NoteForm
 # from .serializers import NoteSerializer, BlogPostSerializer
+# from rest_framework import viewsets
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from .models import Note, BlogPost
+from .forms import *
 
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('notes:login') # accounts/profile error occured here 2/18/24 1 AM CT
+    else:
+        form = RegisterForm()  
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('home'))  # TO-DO: Redirect to success page, or add a success message
+        else:
+            print('Invalid login') # TO-DO: Add error message, session handling logic
+    
+    # else: method is GET        
+    return render(request, 'registration/login.html')  
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')  # TO-DO: Redirect to success page, or add a success message
+
+# TO-DO: Convert these class-based views to function-based views
 class NoteListView(ListView):
     model = Note
     template_name = 'note_list.html'  
@@ -18,6 +51,7 @@ class NoteDetailView(DetailView):
 def home(request):
     return render(request, 'home.html')  
 
+@login_required
 def create_note(request):
     if request.method == 'POST':
         form = NoteForm(request.POST)
