7b5cc1c3bf1ce1943ce89d9331eb98dc093f1ce4
Samuel Pope <spope@inrsenterprises.com>
2024-02-17 20:57:11 -0600

    Implemented frontend for Dev Server, adjusted backend logic accordingly.

--- notes/models.py
+++ notes/models.py
@@ -5,6 +5,7 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
 
