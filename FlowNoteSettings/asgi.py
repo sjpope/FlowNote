98f8a27c35318c909f968663a5933c529698510e
Samuel J Pope <sjpope03@gmail.com>
2024-02-27 18:21:40 -0600

    SCRUM-73  ModuleNotFoundError returned after merging team branches

--- FlowNoteSettings/asgi.py
+++ FlowNoteSettings/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flownote.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FlowNoteSettings.settings')
 
 application = get_asgi_application()
--- FlowNoteSettings/settings.py
+++ FlowNoteSettings/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'flownote.urls'
+ROOT_URLCONF = 'FlowNoteSetting.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'flownote.wsgi.application'
+WSGI_APPLICATION = 'FlowNoteSetting.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
