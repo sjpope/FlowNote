826b451ba1ff3aba2d0e9efc3bf3719badd6e9c9
Samuel J Pope <sjpope03@gmail.com>
2024-04-05 21:53:34 -0500

    Improved handling of analysis failure

--- notes/views.py
+++ notes/views.py
@@ -33,14 +33,19 @@ def analyze(request, note_id):
             # analyze_note_task.delay(note_id)  # TO-DO: Trigger the Celery task to analyze the note asynchronously
 
             result = perform_note_analysis(note_id)
+            
+            if 'Summary' not in result: # Likely caused by note content less than 25 words.
+                return JsonResponse({'result': result})
+            
             keywords, summary = result.split('Summary: ')
             keywords = keywords.replace('Keywords: ', '')
 
             print('Analysis Complete: ', JsonResponse({'keywords': keywords, 'summary': summary}))
 
-            #return redirect('notes:note_detail', pk=note.pk) 
+            # return redirect('notes:note_detail', pk=note.pk) 
             return JsonResponse({'keywords': keywords, 'summary': summary})
-        return JsonResponse({'error': 'Invalid request'}, status=400)
+        else:
+            return JsonResponse({'error': 'Invalid request'}, status=400)
     except Exception as e:
         # TO-DO: Display the error msg as the analysis result instead of sending to notes/pk/analyze
         return JsonResponse({'error': str(e)})
