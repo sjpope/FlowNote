d27d234189bb49f651a25401e0268cf54c5136b2
Samuel J Pope <sjpope03@gmail.com>
2024-04-10 22:04:37 -0500

    Refactor settings.py to add logging configuration

--- FlowNoteSettings/settings.py
+++ FlowNoteSettings/settings.py
@@ -59,6 +59,24 @@ INSTALLED_APPS = [
     
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+
 # Q_CLUSTER = {
 #     'name': 'DjangORM',
 #     'workers': 4,
