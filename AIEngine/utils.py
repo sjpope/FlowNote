6d53219a92132b3f525701ced32354097bf80cf4
Samuel J Pope <sjpope03@gmail.com>
2024-04-12 22:59:37 -0500

    SCRUM-129 Refactor AIEngine/analyze.py: Update content generation and remove unused code.

--- AIEngine/utils.py
+++ AIEngine/utils.py
@@ -1,20 +1,28 @@
 import re
+import string
 import spacy
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+
 from django.core.cache import cache
+
 nlp = spacy.load('en_core_web_sm')
 
 def preprocess_text(text):
+    
     doc = nlp(text)
     lemmatized = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct and not token.is_space]
+    
     return " ".join(lemmatized)
 
 def strip_html_tags(input_string):
+    
     html_tag_pattern = re.compile('<.*?>')
+    
     return html_tag_pattern.sub('', input_string)
 
-def remove_prompt_from_content(prompt, content):
+def strip_prompt(prompt, content):
     try:
         start_index = content.index(prompt) + len(prompt)
     except ValueError:
@@ -22,6 +30,7 @@ def remove_prompt_from_content(prompt, content):
     return content[start_index:].strip()
 
 def get_preprocessed_content(note):
+    
     cache_key = f"preprocessed_{note.pk}"
     preprocessed_content = cache.get(cache_key)
 
