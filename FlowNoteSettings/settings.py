11df6821f1b82671b2e2dbaf9a64315acafc4326
Samuel J Pope <spope@inrsenterprises.com>
2024-04-19 04:59:43 -0500

    SCRUM-148 Updating .gitignore and settings.py

--- FlowNoteSettings/settings.py
+++ FlowNoteSettings/settings.py
@@ -5,6 +5,12 @@ from django.urls import reverse_lazy
 import dj_database_url
 load_dotenv()
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
 HF_HOME=os.environ.get('HF_HOME')
 # CELERY_BROKER_URL = 'redis://localhost:6379/0' # Celery is using 'Redis' as broker
