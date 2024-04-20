07fcc97ef4ca8fb6e4e0885cf2981853424c368c
Samuel J Pope <sjpope03@gmail.com>
2024-04-19 23:50:12 -0500

    SCRUM-148 Fix loading GPT-2 tokenizer and model in config.py

--- AIEngine/config.py
+++ AIEngine/config.py
@@ -1,5 +1,6 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import os
+
 """ Load GPT-2 Model and Tokenizer Globally """
 
 tokenizer = GPT2Tokenizer.from_pretrained(os.getenv('TOKENIZER_PATH'))
